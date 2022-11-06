0576b1b5e3c8d6d351588e4f7b40add44e483105
Vern <vern.yi@besquare.com.my>
2022-11-06 10:30:07 +0000

    update route and navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:drc_cryptown/pages/exchange-list.dart';
+import 'package:drc_cryptown/pages/news.dart';
 import 'package:drc_cryptown/states/crypto/crypto-cubit.dart';
+import 'package:drc_cryptown/states/exchange-list/exchange-list-cubit.dart';
+import 'package:drc_cryptown/states/news/news-cubit.dart';
 import 'package:drc_cryptown/pages/crypto-list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,6 +10,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() {
   runApp(MultiBlocProvider(providers:[
     BlocProvider(create: (context) => CryptoCubit()),
+    BlocProvider(create: (context) => NewsCubit()),
+    BlocProvider(create: (context) => ExchangeCubit()),
   ],child: const MyApp()));
 }
 
@@ -13,6 +19,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,9 +34,14 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.lightBlue,
       ),
       home: const CryptoPage(),
+      routes: {
+        '/crypto-list=':(context) => CryptoPage(),
+        '/news-list':(context) => NewsPage(),
+        '/exchange-list':(context) => ExchangeList(),
+  },
     );
   }
 }
--- lib/widgets/nav-bar.dart
+++ lib/widgets/nav-bar.dart
@@ -6,52 +6,56 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Text(
-              'Cryptown',
-              style: TextStyle(color: Colors.black, fontSize: 25),
+      child:Container(
+        color: Color.fromRGBO(133, 191, 255, 100),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Cryptown',
+                style: TextStyle(color: Colors.black, fontSize: 25),
+              ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('Crypto'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.star),
-            title: Text('Watchlist'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.tips_and_updates),
-            title: Text('Exchange List'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.forum),
-            title: Text('Forum'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.newspaper),
-            title: Text('News'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('Profile Settings'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Logout'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-        ],
-      ),
+            ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('Crypto'),
+              onTap: () => {Navigator.of(context).pushNamed('/crypto-list')},
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('Watchlist'),
+              onTap: () => {Navigator.of(context).pushNamed('/crypto-list')},
+            ),
+            ListTile(
+              leading: Icon(Icons.tips_and_updates),
+              title: Text('Exchange List'),
+              onTap: () => {Navigator.of(context).pushNamed('/exchange-list')},
+            ),
+            ListTile(
+              leading: Icon(Icons.forum),
+              title: Text('Forum'),
+              onTap: () => {Navigator.of(context).pushNamed('/crypto-list')},
+            ),
+            ListTile(
+              leading: Icon(Icons.newspaper),
+              title: Text('News'),
+              onTap: () => {Navigator.of(context).pushNamed('/news-list')},
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile Settings'),
+              onTap: () => {Navigator.of(context).pushNamed('/crypto-list')},
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () => {Navigator.of(context).pushNamed('/crypto-list')},
+            ),
+          ],
+        ),
+      )
+
     );
   }
 }
