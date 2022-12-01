da6a52057b23d95a2433b61ccb1e2c5dc1ce4346
Vern <vern.yi@besquare.com.my>
2022-12-01 08:22:34 +0000

    update watchlist no coin screen

--- lib/widgets/watch-list.dart
+++ lib/widgets/watch-list.dart
@@ -48,78 +48,92 @@ Future<Map<String, dynamic>> getWatchlist() async {
            //get json response body data into list
            var watchlist = snapshot.data!["favourites"];
            List<Favourite> favourite = List.castFrom(watchlist.map((e) => Favourite.fromMap(e)).toList());
-
-           return ListView.builder(
-               itemCount: favourite.length,
-               itemBuilder: (context, index) {
-                 return Card(
-                     color: Color.fromRGBO(127, 156, 200, 1.0),
-                     child: Container(
-                       padding: EdgeInsets.symmetric(horizontal: 15),
-                       child:InkWell(
-                         onTap: (){
-                           Navigator.of(context).pushNamed(
-                               '/crypto-details',
-                               arguments:favourite[index].cryptoid);
-                         },
-                         child: Padding(
-                           padding: const EdgeInsets.all(8.0),
-                           child: Row(
+           print(snapshot.data);
+           if (favourite.isEmpty){
+             return Container(
+               alignment: Alignment.topCenter,
+               padding: EdgeInsets.all(20),
+               child: Text('No crypto added into Watchlist',
+                 style: TextStyle(
+                   color: Colors.white60,
+                   fontSize: 24,
+                   fontWeight: FontWeight.w400,
+                 ) ,),
+             );
+           } else{
+             return ListView.builder(
+                 itemCount: favourite.length,
+                 itemBuilder: (context, index) {
+                   return Card(
+                       color: Color.fromRGBO(127, 156, 200, 1.0),
+                       child: Container(
+                         padding: EdgeInsets.symmetric(horizontal: 15),
+                         child:InkWell(
+                           onTap: (){
+                             Navigator.of(context).pushNamed(
+                                 '/crypto-details',
+                                 arguments:favourite[index].cryptoid);
+                           },
+                           child: Padding(
+                             padding: const EdgeInsets.all(8.0),
+                             child: Row(
                                mainAxisSize: MainAxisSize.min,
                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                               children: [
+                                 Row(
                                    children: [
-                                     Row(
-                                       children: [
-                                         Text(favourite[index].coinname,
-                                           style: TextStyle(
-                                             fontSize: 14,
-                                             fontWeight: FontWeight.bold,
-                                           ) ,),
-                                         const SizedBox(width: 10),
-                                         Image.network(favourite[index].imageUrl,height: 40,),
-                                       ],
-                                     ),
-                                     ElevatedButton(
-                                         onPressed: (){
-                                           setState(() async {
-                                             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                               content: const Text('Removing coin from WatchList'),
-                                               backgroundColor: Colors.green.shade300,
-                                             ));
-                                             Map<String, dynamic> favCoinData = {
-                                               'favId' : favourite[index].favid,
-                                             };
-
-                                             dynamic res = await watchlistService.deleteWatchListCoin(accessToken: accesstoken,data: favCoinData);
-
-                                             if (res['deletedFavId'] != null) {
-                                               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                                 content: const Text('Removed from Watchlist'),
-                                                 backgroundColor: Colors.green.shade300,
-                                               ));
-                                               Navigator.of(context).pushNamed('/watch-list');
-                                             } else {
-                                               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                                 content: Text('Error: ${res['mssg']}'),
-                                                 backgroundColor: Colors.red.shade300,
-                                               ));
-                                             }
-                                           });
-                                         },
-                                         child: Text(
-                                           'remove'
-                                         ))
+                                     Text(favourite[index].coinname,
+                                       style: TextStyle(
+                                         fontSize: 14,
+                                         fontWeight: FontWeight.bold,
+                                       ) ,),
+                                     const SizedBox(width: 10),
+                                     Image.network(favourite[index].imageUrl,height: 40,),
                                    ],
                                  ),
-                         ),
+                                 ElevatedButton(
+                                     onPressed: (){
+                                       setState(() async {
+                                         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                           content: const Text('Removing coin from WatchList'),
+                                           backgroundColor: Colors.green.shade300,
+                                         ));
+                                         Map<String, dynamic> favCoinData = {
+                                           'favId' : favourite[index].favid,
+                                         };
+
+                                         dynamic res = await watchlistService.deleteWatchListCoin(accessToken: accesstoken,data: favCoinData);
+
+                                         if (res['deletedFavId'] != null) {
+                                           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                             content: const Text('Removed from Watchlist'),
+                                             backgroundColor: Colors.green.shade300,
+                                           ));
+                                           Navigator.of(context).pushNamed('/watch-list');
+                                         } else {
+                                           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                             content: Text('Error: ${res['mssg']}'),
+                                             backgroundColor: Colors.red.shade300,
+                                           ));
+                                         }
+                                       });
+                                     },
+                                     child: Text(
+                                         'remove'
+                                     ))
+                               ],
+                             ),
+                           ),
                          ),
                        )
-                 );
-               }
-           );
-          } else {
+                   );
+                 }
+             );
+           }
+          }
+          else {
             debugPrint(snapshot.error.toString());
-         }
+          }
           return const SizedBox();
         }),
       )
